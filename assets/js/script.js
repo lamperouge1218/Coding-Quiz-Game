8a4019aad3594dc639b3fd09f8dfc6b25b0f97ae
lamperouge <philbohn1791@gmail.com>
2021-09-13 16:57:45 -0400

    added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@
 // to wipe all text from these on button press
 var startPageClass = document.querySelector(".startpage");
 var quizStartButton = document.querySelector("#startbutton");
+var timeEl = document.querySelector(".time");
 
 // Creating mainEl variable so that data can be appended to it
 // later on in this process
@@ -44,45 +45,21 @@ var question5 = {
     correctAnswer: "console.log"
 }
 
+var timeLeft = 70;
 
-// Upon clicking a button, I want to go to the next question
-// If that button is the wrong answer, remove 10 seconds from the timer and go to next
-// If that button is the correct answer, go to the next question 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timeEl.textContent = "Time: " + timeLeft;
+        if(timeLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
 
-
-// Add an event listener that will remove current text on the screen and
-// add new text upon click
-
-// quizStartButton.addEventListener("click", function () {
-//     mainEl.textContent = " ";
-
-//     var question1 = document.createElement("h2");
-//     var oneChoice1 = document.createElement("button");
-//     var oneChoice2 = document.createElement("button");
-//     var oneChoice3 = document.createElement("button");
-//     var oneChoice4 = document.createElement("button");
-
-//     oneChoice1.textContent = ("1. strings");
-//     oneChoice2.textContent = ("2. booleans");
-//     oneChoice3.textContent = ("3. alerts");
-//     oneChoice4.textContent = ("4. numbers");
-
-    // question1.textContent = ("Commonly used data types DO NOT include:");
-
-//     question1.setAttribute("style", "display: flex; flex-direction: column;");
-//     oneChoice1.setAttribute("style", "background-color: purple; border-radius: 5px; font-size: 15px; color: white; font-weight: bold; text-align: left;");
-//     oneChoice2.setAttribute("style", "background-color: purple; border-radius: 5px; font-size: 15px; color: white; font-weight: bold; text-align: left;");
-//     oneChoice3.setAttribute("style", "background-color: purple; border-radius: 5px; font-size: 15px; color: white; font-weight: bold; text-align: left;");
-//     oneChoice4.setAttribute("style", "background-color: purple; border-radius: 5px; font-size: 15px; color: white; font-weight: bold; text-align: left;")
-
-//     mainEl.appendChild(question1);
-//     question1.appendChild(oneChoice1);
-//     question1.appendChild(oneChoice2);
-//     question1.appendChild(oneChoice3);
-//     question1.appendChild(oneChoice4);
-// })
 
 quizStartButton.addEventListener("click", function () {
+    setTime();
     mainEl.textContent = " ";
 
     var question1Text = document.createElement("h2");
@@ -112,6 +89,14 @@ quizStartButton.addEventListener("click", function () {
 
    
 })
+
+// Upon clicking a button, I want to go to the next question
+// If that button is the wrong answer, remove 10 seconds from the timer and go to next
+// If that button is the correct answer, go to the next question 
+
+
+
+
 
 
 // In JS, will need to create, add content, style properly, and append
